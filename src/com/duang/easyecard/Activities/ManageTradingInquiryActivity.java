16bc641ab74ad083c6b44eff4d904960befa1ae5
SunGoodBoy <361574485@qq.com>
2015-12-20 12:00:29 +0800

    Add ActionBar tabs for ManageTradingInquiryActivity.

--- src/com/duang/easyecard/Activities/ManageTradingInquiryActivity.java
+++ src/com/duang/easyecard/Activities/ManageTradingInquiryActivity.java
@@ -1,5 +1,50 @@
 package com.duang.easyecard.Activities;
 
+import com.duang.easyecard.R;
+import com.duang.easyecard.Utils.TabListener;
+
+import android.app.ActionBar;
+import android.app.ActionBar.Tab;
+import android.os.Bundle;
+
 public class ManageTradingInquiryActivity extends BaseActivity {
 	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_manage_trading_inquiry);
+		
+		initView();
+	}
+
+	private void initView() {
+		// TODO Auto-generated method stub
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
+		actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		
+		// 添加Tab选项
+		Tab tab = actionBar.newTab()
+				.setText("历史流水")
+				.setTabListener(
+						new TabListener<ManageTradingInquiryHistoryFragment>(
+								this, "历史流水", ManageTradingInquiryHistoryFragment.class));
+		actionBar.addTab(tab);
+		
+		tab = actionBar.newTab()
+				.setText("当日流水")
+				.setTabListener(
+						new TabListener<ManageTradingInquiryDayFragment>(
+								this, "当日流水", ManageTradingInquiryDayFragment.class));
+		actionBar.addTab(tab);
+		
+		tab = actionBar.newTab()
+				.setText("当月流水")
+				.setTabListener(
+						new TabListener<ManageTradingInquiryMonthFragment>(
+								this, "当月流水", ManageTradingInquiryMonthFragment.class));
+		actionBar.addTab(tab);
+	}
+
 }
---
+++ src/com/duang/easyecard/Activities/ManageTradingInquiryHistoryFragment.java
@@ -0,0 +1,18 @@
+package com.duang.easyecard.Activities;
+
+import com.duang.easyecard.R;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ManageTradingInquiryHistoryFragment extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.fragment_trading_inquiry_history, container, false);
+	}
+}
