f51b13d9e0bd9f8dc071c91ff2dbd5e2c3079a40
SunGoodBoy <361574485@qq.com>
2015-12-23 23:58:14 +0800

    Success to send POST request to TrjnQuery.

--- src/com/duang/easyecard/Activities/ManageTradingInquiryActivity.java
+++ src/com/duang/easyecard/Activities/ManageTradingInquiryActivity.java
@@ -1,18 +1,32 @@
 package com.duang.easyecard.Activities;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
 import com.duang.easyecard.R;
 import com.duang.easyecard.Activities.ManageTradingInquiryHistoryFragment.MyCallback;
+import com.duang.easyecard.GlobalData.MyApplication;
+import com.duang.easyecard.GlobalData.UrlConstant;
+import com.duang.easyecard.Utils.LogUtil;
 import com.duang.easyecard.Utils.TabListener;
 
 import android.app.ActionBar;
 import android.app.ActionBar.Tab;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class ManageTradingInquiryActivity extends BaseActivity 
 implements MyCallback{
@@ -20,10 +34,16 @@ implements MyCallback{
 	// HISTORY_STATE用于区别Tab(0)加载的Fragment类型 {0:select time; 1:result}
 	private static int HISTORY_STATE = 0;  // 初始为选择时间Fragment
 	
-	protected static int startYear, startMonthOfYear, startDayOfMonth, startDayOfWeek;
-	protected static int endYear, endMonthOfYear, endDayOfMonth, endDayOfWeek;
+	private HttpClient httpClient;
+	
+	protected static int startYear, startMonthOfYear, startDayOfMonth;
+	protected static int startDayOfWeek;
+	protected static int endYear, endMonthOfYear, endDayOfMonth;
+	protected static int endDayOfWeek;
 	protected static String startTime;
 	protected static String endTime;
+	
+	private final int POST_SUCCESS_RESPONSE = 200;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +52,8 @@ implements MyCallback{
 		setContentView(R.layout.activity_manage_trading_inquiry);
 
 		initView();
+		
+		initData();
 	}
 
 	private void initView() {
@@ -66,6 +88,63 @@ implements MyCallback{
 		actionBar.addTab(tab);
 	}
 
+	private void initData() {
+		// 获得全局变量httpClient
+		MyApplication myApp = (MyApplication) getApplication();
+		httpClient = myApp.getHttpClient();
+		// 发送POST信息，开始流水查询
+		sendPostRequest();
+	}
+	// 处理从线程中传递出来的消息
+	private Handler handler = new Handler() {
+		public void handleMessage(Message msg) {
+			switch (msg.what) {
+			case POST_SUCCESS_RESPONSE:
+				LogUtil.d("POST_SUCCESS_RESPONSE", msg.obj.toString());
+				// 刷新全局httpClient
+				MyApplication myApp = (MyApplication) getApplication();
+				myApp.setHttpClient(httpClient);
+				break;
+			default:
+				break;
+			}
+		}
+	};
+	// 发送POST请求
+	private void sendPostRequest() {
+		// 发送POST信息到TRJN_QUERY，开始流水查询
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				// 创建一个HttpPost对象
+				HttpPost httpPost = new HttpPost(UrlConstant.TRJN_QUERY);
+				List<NameValuePair> params = new ArrayList<NameValuePair>();
+				try {
+					params.add(new BasicNameValuePair("needHeader", "false"));
+					UrlEncodedFormEntity entity = new UrlEncodedFormEntity(
+							params, "utf-8");
+					httpPost.setEntity(entity);
+					// 发送POST请求
+					HttpResponse httpResponse = httpClient.execute(httpPost);
+					// 如果服务器成功地返回响应
+					if (httpResponse.getStatusLine().getStatusCode() == 200) {
+						// 请求和响应都成功了
+						LogUtil.d("POST", "Success!");
+						// 获取返回的cookies
+				        String httpResponseString = EntityUtils.toString(
+				        		httpResponse.getEntity());
+				        Message message = new Message();
+						message.what = POST_SUCCESS_RESPONSE;
+						message.obj = httpResponseString;
+						handler.sendMessage(message);
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
+
 	/*
 	 * (non-Javadoc)
 	 * @see com.duang.easyecard.Activities.ManageTradingInquiryHistoryFragment
@@ -76,8 +155,10 @@ implements MyCallback{
 	@Override
 	public void onBtnClick(View v) {
 		// 向搜索结果Fragment传递startTime和endTime
-		TextView startTimeText = (TextView) findViewById(R.id.history_inquiry_start_time);
-		TextView endTimeText = (TextView) findViewById(R.id.history_inquiry_end_time);
+		TextView startTimeText = (TextView) findViewById(
+				R.id.history_inquiry_start_time);
+		TextView endTimeText = (TextView) findViewById(
+				R.id.history_inquiry_end_time);
 		startTime = startTimeText.getText().toString();
 		endTime = endTimeText.getText().toString();
 		// 更换Tab(0)的布局
@@ -85,7 +166,8 @@ implements MyCallback{
 		Tab tab = actionBar.getTabAt(0);
 		actionBar.removeTabAt(0);
 		tab.setText("历史流水")
-		.setTabListener(new TabListener<ManageTradingInquiryHistoryResultFragment>(
+		.setTabListener(new TabListener<
+				ManageTradingInquiryHistoryResultFragment>(
 				this, "历史流水", ManageTradingInquiryHistoryResultFragment.class));
 		actionBar.addTab(tab, 0);
 		actionBar.selectTab(tab);
@@ -123,9 +205,6 @@ implements MyCallback{
 				actionBar.addTab(tab, 0);
 				actionBar.selectTab(tab);
 				// 将搜索界面选择的时间赋给startTime和endTime
-				// Button btn = (Button) findViewById(R.id.fragment_trading_inquiry_history_result_btn);
-				// String str = btn.getText().toString();
-				// Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
 				HISTORY_STATE = 0;  // 状态置为选择时间Fragment
 			}
 		} else {
