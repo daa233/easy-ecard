ec3f4aa652bacded51d549317c55d8f556f129e6
SunGoodBoy <361574485@qq.com>
2016-01-01 19:57:00 +0800

    Add ManageReportLossActivity & Extract HttpUtil.

--- src/com/duang/easyecard/Activities/ManageReportLossActivity.java
+++ src/com/duang/easyecard/Activities/ManageReportLossActivity.java
@@ -1,3 +1,7 @@
+package com.duang.easyecard.Activities;
+
+import android.os.Bundle;
+
 public class ManageReportLossActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
