fb8e2a2439780e0503c5de9e3e8a59ee79b7e09a
SunGoodBoy <361574485@qq.com>
2016-01-16 19:49:20 +0800

    Add lost_and_found_info_browsing_list_item.xml

--- src/com/duang/easyecard/Activities/LostAndFoundInformationBrowsingActivity.java
+++ src/com/duang/easyecard/Activities/LostAndFoundInformationBrowsingActivity.java
@@ -1,5 +1,28 @@
 package com.duang.easyecard.Activities;
 
-public class LostAndFoundInformationBrowsingActivity extends BaseActivity {
+import com.duang.easyecard.R;
 
+import android.os.Bundle;
+
+public class LostAndFoundInformationBrowsingActivity extends BaseActivity {
+	
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_lost_and_found_information_browsing);
+		
+		initView();
+		
+		initData();
+	}
+
+	private void initView() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void initData() {
+		// TODO Auto-generated method stub
+		
+	}
 }
