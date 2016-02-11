97e1f68500ba44f814275a8ca6d03e9f0e759e33
SunGoodBoy <361574485@qq.com>
2016-02-11 21:03:04 +0800

    Add MessageConstant.

---
+++ app/src/main/java/com/duang/easyecard/GlobalData/MessageConstant.java
@@ -0,0 +1,18 @@
+package com.duang.easyecard.GlobalData;
+
+/**
+ * Created by MrD on 2016/2/11.
+ */
+public class MessageConstant {
+    public final static int SIGNIN_SUCCESS = 1;
+    public final static int SIGNIN_FAILED = 2;
+    private final static int GET_RESPONSE_SUCCESS = 3;
+    private final static int POST_RESPONSE_SUCCESS = 4;
+    public final static int NEED_MORE_DATA = 5;
+    public final static int NETWORK_ERROR = 6;
+    public final static int FINISH_HISTORY_LIST = 7;
+    public final static int FINISH_WEEK_LIST = 8;
+    public final static int FINISH_DAY_LIST = 9;
+    public final static int REDAY_TO_SEND_GET_REQUEST = 10;
+    public final static int READY_TO_SEND_POST_REQUEST = 11;
+}
