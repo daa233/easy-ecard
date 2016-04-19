b70255218c87f987e959d056828053c04260679c
SunGoodBoy <361574485@qq.com>
2016-04-19 23:43:02 +0800

    Add AppStartActivity. Unfinished yet.

---
+++ app/src/main/java/com/duang/easyecard/Activity/AppStartActivity.java
@@ -0,0 +1,15 @@
+package com.duang.easyecard.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.duang.easyecard.R;
+
+public class AppStartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_start);
+    }
+}
